be242bdf3f852e74c81ee60ac8deaa3f0e8946c5
jcastro506 <castrojoshua805.com>
2020-11-17 16:42:39 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,18 @@ class StudentsController < ApplicationController
   end
 
   def new
+    render :form
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
+    @student.save
   end
 
 end
